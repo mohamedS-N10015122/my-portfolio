79699712b5b3583e49235dea878f67154dc89bde
mohamedS <salad2692@gmail.com>
2026-04-21 17:01:44 -0400

    added funtionality to store messages

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const form = document.getElementById('form event')
 const username = document.getElementById('username')
 const message = document.getElementById('message')
+let messageLog = []
 form.addEventListener('submit', function (event){
     if (username.value.trim() === '' || message.value.trim() === ''){
         alert("One or more fields are empty.")
     } else if (username.value.trim() !== '' && message.value.trim() !== ''){
         alert("Submitted")
+        messageLog.push({username: username.value, message: message.value})
     }
     event.preventDefault()
 })
