3170b3fa0133477723b2acce0c089c7ee9ce4d0a
mohamedS <salad2692@gmail.com>
2026-04-21 20:41:53 -0400

    added date to contact array

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // form logic
-const form = document.getElementById('form event')
-const username = document.getElementById('username')
-const message = document.getElementById('message')
+const form = document.getElementById('form event');
+const username = document.getElementById('username');
+const message = document.getElementById('message');
 let messageLog = []
 form.addEventListener('submit', function (event){
     if (username.value.trim() === '' || message.value.trim() === ''){
-        alert("One or more fields are empty.")
+        alert("One or more fields are empty.");
     } else if (username.value.trim() !== '' && message.value.trim() !== ''){
-        alert("Submitted")
-        messageLog.push({username: username.value, message: message.value})
+        alert("Submitted");
+        messageLog.push({username: username.value, message: message.value, date: Date()});
     }
-    event.preventDefault()
+    event.preventDefault();
 })
 // dark mode logic
 function toggle() {
-    let button = document.getElementsByClassName("toggle-btn")
+    let button = document.getElementsByClassName("toggle-btn");
     if (button[0].textContent === "Light mode"){
         button[0].textContent = "Dark mode"
-          localStorage.setItem("dark mode", false) // used that if you refresh after changing contrast it doesn't change
+          localStorage.setItem("dark mode", false); // used that if you refresh after changing contrast it doesn't change
     } else {
         button[0].textContent = "Light mode"
-          localStorage.setItem("dark mode", true)
+          localStorage.setItem("dark mode", true);
     }
   var element = document.body;
   element.classList.toggle("light-mode");
 
 }
 if (localStorage.getItem('dark mode') === "false") {
-    toggle()
+    toggle();
 }
